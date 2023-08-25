f9b467eb177aa26b36bef5c825563bee2fa2c81d
Aiman Zandinan <azandinan2@huskers.unl.edu>
2023-08-25 16:50:13 -0500

    I added my name and major

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv) {
 
   printf("Computer Engineering\n");
   printf("Aayush Subedi!\n");
+  printf("Aiman Zandinan\n");
+  printf("I am a Computer Science major\n");
 
   return 0;
 }
